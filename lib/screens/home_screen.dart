d70d6017c714638fd8a35115c77b3f6845e89f3b
Mohammad Areeb Kashaf <areebkashaf7350@gmail.com>
2024-04-24 00:32:39 +0530

    fetching candidates realtime

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,12 +13,13 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: SafeArea(
         child: ListView.builder(
+          physics: const NeverScrollableScrollPhysics(),
           itemCount: 3,
           itemBuilder: (context, index) => Padding(
             padding: const EdgeInsets.all(8.0),
             child: ListTile(
               shape: RoundedRectangleBorder(
-                  side: const BorderSide(color: Colors.grey),
+                  side: const BorderSide(color: Colors.black),
                   borderRadius: BorderRadius.circular(8.0)),
               leading: Image.network(
                 "https://i.pinimg.com/originals/b4/82/e8/b482e8d9178debd58e1d10b656397319.png",
